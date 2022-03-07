d6d7f4fc79696d32b3df287d6da66ac43c4deee0
Suraj Vishwakarma <93025576+Suraj9850@users.noreply.github.com>
2022-03-07 10:01:46 +0530

    Delete practical Slip directory

--- practical Slip/slip9_py1.py
+++
@@ -1,9 +0,0 @@
-# Write a Python script using class to reverse a string word by word
-
-class string:
-    def rev(self,s):
-        word=s.split(' ')
-        reve=' '.join(reversed(word))
-        print("Revesed String:",reve)
-a=string()
-a.rev("Hello How Are you")
